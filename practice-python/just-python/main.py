13970cc575c767f34be95046675bdfe17f91b1f5
hyde0401 <hyde0401@nm-patner.com>
2022-11-08 16:48:13 +0900

    feat : built-in data structure

--- practice-python/just-python/main.py
+++ practice-python/just-python/main.py
@@ -1,1 +1,21 @@
-print("hello world")
+# built-in data structure
+print(type(1)) # int
+print(type(1.1)) # float
+print(type(4 + 1j)) # complex
+print(type("hello")) # str
+print(type('hello')) # str
+print(type(True)) # boolean
+print(type(False)) # boolean
+print("hello\nhello")
+print(
+"""hi
+hi"""
+)
+print(1==1)
+print(type(1==1))
+print(1!=1)
+print(type(1!=1))
+print(int(True))
+print(1==True)
+print(int(False))
+print(0==False)
